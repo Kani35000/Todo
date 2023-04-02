eb0b2f59c476765a1531b5fb19b3320934a57ce3
Kanidaye Okorji <kanidayeokorji@gmail.com>
2023-04-02 19:50:59 +0200

    edited app.py

--- app.py
+++ app.py
@@ -15,17 +15,20 @@ debug = DebugToolbarExtension(app)
 connect_db(app)
 
 @app.route('/')
-def index_page():
+def homepage():
     todos= Todo.query.all()
     return render_template('todo2.html', todos=todos)
 
-@app.route('/api/todos', methods=["POST"])
+
+@app.route('/', methods = ['POST'])
 def create_todo():
-    new_todo= Todo(title=request.json['title'])
+    content= request.form.get("content")
+    category= request.form.get("category")
+    todos= Todo.query.all()
+    new_todo= Todo(content= content, category= category)
     db.session.add(new_todo)
     db.session.commit()
-    response_json= jsonify(todo=new_todo.serialized())
-    return (response_json, 201) 
+    return render_template('todo2.html', todos=todos, new_todo= new_todo)
 
 
 @app.route('/api/todos')
@@ -39,7 +42,7 @@ def get_todo(id):
     return jsonify(todo=todo.serialized())
 
 @app.route('/api/todos', methods=["POST"])
-def create_todo():
+def create_todo_api():
     new_todo= Todo(title=request.json['title'])
     db.session.add(new_todo)
     db.session.commit()
@@ -47,7 +50,7 @@ def create_todo():
     return (response_json, 201) 
 
 @app.route('/api/todos/<int:id>', methods=["PATCH"])
-def update_todo(id):
+def update_todo_api(id):
     todo= Todo.query.get_or_404(id)
     todo.title=request.json.get['title',todo.title]
     todo.done=request.json.get['done',todo.done]
